4bc5df76b0547a22ac823cb6acb2aa940445dccf
Muhammad Faizan Ahmad <muhammadfaizanahmad01@gmail.com>
2024-08-05 22:35:45 +0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,229 @@
+// VARIABLES
+
+// 1- var
+// 2- let
+// 3- const
+
+// VAR
+
+// var myStudent = "Fazal";
+// console.log(myStudent);
+
+// RE-DECLARED
+
+// var myStudent = "Ahmad"
+// console.log(myStudent);
+
+// RE - ASSIGNED
+
+// myStudent = "Abdul Rahman";
+// console.log(myStudent);
+
+// LET
+
+// let myLanguage = "Javascript"
+// console.log(myLanguage);
+
+// RE-DECLARED
+
+// let myLanguage = "Python";
+// console.log(myLanguage);
+
+// RE - ASSIGNED
+
+// myLanguage = "Python"
+// console.log(myLanguage);
+
+// CONST
+
+// const mySports = "Soccer";
+// console.log(mySports);
+
+// RE-DECLARED
+
+// const mySports = "Cricket";
+// console.log(mySports);
+
+// RE - ASSIGNED
+
+// mySports = "Soccer";
+// console.log(mySports);
+
+// DATA TYPES
+
+// 1- STRING
+// 2- NUMBER
+// 9 -BIGINT
+// 3- BOOLEAN
+// 4- ARRAYS
+// 5- OBJECTS
+// 6- NULL
+// 7- UNDEFINED
+// 8- SYMBOL
+
+// var a = "Laptop";
+// var price = 500;
+// var isNew = true;
+// var c;
+// var d = null;
+
+// var cars = {
+//   brand: "toyota",
+//   model: 2020,
+//   isUsed: true,
+// };
+
+// const fruits = ["mango", "1KG", 120];
+
+// console.log(typeof a);
+// console.log(typeof price);
+// console.log(typeof isNew);
+// console.log(typeof c);
+// console.log(typeof d);
+// console.log(typeof(cars));
+// console.log(typeof fruits);
+
+// OPERATORS
+
+// 1- Arithmetic Operators
+
+// -> Addition
+// -> Subtraction
+// -> Multiplication
+// -> Division
+// -> Modulus
+// -> Increment
+// -> Decrment
+// -> Exponentation
+
+// var a = 10;
+// var b = 5;
+// var c = a % b;
+// console.log(c);
+
+// var count = 5;
+
+// ++count;
+
+// console.log(count);
+
+// console.log(count++);
+
+// console.log(count);
+
+// --count;
+
+// console.log(count);
+
+// console.log(count--);
+
+// console.log(count);
+
+//Exponentation Operator
+
+// let base = 2;
+// let exponentation = 3;
+
+// let result = base ** exponentation;
+// console.log(result);
+
+// 2- ASSIGNMENT OPERATORS
+
+// let x = 5; // Assignment: x is now 5
+// x += 5; // Addition assignment: x
+// x = x + 5;
+// console.log(x);
+
+// Comparison Operators
+
+// 1- == (Equal to)
+// 2- === (Strict Equal to)
+// 3- != (Not Equal to)
+// 4- !== (Strict not Equal to)
+// 5- > (Greater than)
+// 6- >= (Greater than or equal to)
+// 7- < (Lesss than)
+// 8- <= (Less than or equal to)
+
+// Logical Operators
+
+// 1- && (AND Operator)
+// 2- || (OR Operator)
+// 3 - ! (NOT Operator)
+
+// CONDITIONAL STATEMENT
+
+// 1- if Statement
+// 2- if else Statement
+// Alternative - Ternary Operator
+// 3-  if else if Statement
+// 4- Switch Case
+
+// var a = 10;
+// var b = 10;
+
+// if (a > b) {
+//   console.log("A is Greater than B");
+// } else {
+//     console.log("A is Less than B")
+// }
+
+// var result = a > b ? "a is greater" : "b is greater";
+// console.log(result);
+
+// SWITCH CASE
+
+// let fruit = "grapes";
+
+// switch (fruit) {
+//   case "apple":
+//     console.log("Apples are $1.99 per pound");
+//     break;
+//   case "banana":
+//     console.log("Banana are $2.39 per pound");
+//     break;
+//   case "mango":
+//     console.log("Mango are $1.44 per pound");
+//     break;
+//   default:
+//     console.log("Sorry we are out of fruits!");
+//     break;
+// }
+
+// LOOPS
+
+// while loop
+
+// 1- Initialization
+// 2- Condition
+// 3- Increment / Decrement
+
+// while loop
+
+// let count = 0;
+
+// while (count < 5) {
+//   console.log(count);
+//   count++;
+// }
+
+// do while loop
+
+// let count = 0;
+
+// do {
+//   console.log(count);
+//   count++;
+// } while (count < 5);
+
+// for loop
+
+// for (let count = 0; count < 5; count++) {
+//   console.log(count);
+// }
+
+// let fruits = ["banana", "apple", "cherry"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
